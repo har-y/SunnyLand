01c9f26c377405a35c1c0102f4e380c0f5277f7f
har-y <jakub.harenda@gmail.com>
2021-01-17 17:40:06 +0100

    add basic player movement to player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody2D _rb;
+    private float _horizontal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontal = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector2(_moveSpeed * _horizontal, _rb.velocity.y);
     }
 }
