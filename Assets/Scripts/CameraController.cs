0d091258bd3aefbf54f9097fe0bf9ec5c53a2676
har-y <jakub.harenda@gmail.com>
2021-01-21 21:03:54 +0100

    add basic parallax background

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private Transform _farBackground;
+    [SerializeField] private Transform _middleBackground;
+
+    private float _xLastPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _xLastPosition = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(_target.position.x, transform.position.y, transform.position.z);
+
+        float xOffset = transform.position.x - _xLastPosition;
+
+        _farBackground.position += new Vector3(xOffset, 0f, 0f);
+        _middleBackground.position += new Vector3(xOffset * 0.5f, 0f, 0f);
+
+        _xLastPosition = transform.position.x;
     }
 }
