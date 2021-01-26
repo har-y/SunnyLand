838fafcbafb62dee0db6c9b128cf3a90e8ee8eea
har-y <jakub.harenda@gmail.com>
2021-01-26 22:46:50 +0100

    add max health and current health to player health controller

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerHealthController : MonoBehaviour
 {
+    [SerializeField] private int _currentHealth;
+    [SerializeField] private int _maxHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentHealth = _maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void DealDamage()
+    {
+        _currentHealth--;
+
+        if (_currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
