f68b12ecdeb7c67805b2d73f209f1c566a90ef38
har-y <jakub.harenda@gmail.com>
2021-02-01 23:47:17 +0100

    add enemy frog animation (2)

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -10,8 +10,15 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private Transform _rightPoint;
 
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _moveTime;
+    [SerializeField] private float _waitTime;
 
     private Rigidbody2D _rb;
+
+    private Animator _animator;
+
+    private float _moveCounter;
+    private float _waitCounter;
 
     private bool _changeDirection;
 
@@ -19,13 +26,51 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
 
         _leftPoint.parent = null;
         _rightPoint.parent = null;
+
+        _moveCounter = _moveTime;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        EnemyRandomMove();
+    }
+
+    private void EnemyRandomMove()
+    {
+        if (_moveCounter > 0)
+        {
+            _moveCounter -= Time.deltaTime;
+
+            EnemyMove();
+
+            if (_moveCounter <= 0)
+            {
+                _waitCounter = Random.Range(_waitTime * 0.75f, _waitTime * 1.25f);
+            }
+
+            _animator.SetBool("isMove", true);
+        }
+        else if (_waitCounter > 0)
+        {
+            _waitCounter -= Time.deltaTime;
+
+            _rb.velocity = new Vector2(0f, _rb.velocity.y);
+
+            if (_waitCounter <= 0)
+            {
+                _moveCounter = Random.Range(_moveTime * 0.75f, _moveTime * 0.75f);
+            }
+
+            _animator.SetBool("isMove", false);
+        }
+    }
+
+    private void EnemyMove()
     {
         if (!_changeDirection)
         {
