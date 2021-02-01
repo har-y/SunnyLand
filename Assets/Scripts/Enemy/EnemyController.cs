139cec4394b0066b239ead6c194f1d64a0963aea
har-y <jakub.harenda@gmail.com>
2021-02-01 20:39:26 +0100

    add enemy frog move from point to point

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer _enemySprite;
+
+    [SerializeField] private Transform _leftPoint;
+    [SerializeField] private Transform _rightPoint;
+
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody2D _rb;
+
+    private bool _changeDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+
+        _leftPoint.parent = null;
+        _rightPoint.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!_changeDirection)
+        {
+            _rb.velocity = new Vector2(-_moveSpeed, _rb.velocity.y);
+
+            _enemySprite.flipX = false;
+
+            if (transform.position.x < _leftPoint.position.x)
+            {
+                _changeDirection = true;
+            }
+        }
+        else
+        {
+            _rb.velocity = new Vector2(_moveSpeed, _rb.velocity.y);
+
+            _enemySprite.flipX = true;
+
+            if (transform.position.x > _rightPoint.position.x)
+            {
+                _changeDirection = false;
+            }
+        }
     }
 }
