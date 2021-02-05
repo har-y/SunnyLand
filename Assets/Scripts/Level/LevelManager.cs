38802bc0f8deafbe736c742f8f92bc7c4c854bbc
har-y <jakub.harenda@gmail.com>
2021-02-05 23:05:09 +0100

    add main menu scroll background

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -18,7 +18,7 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioManager.instance.PlayMusicClip(4);
     }
 
     // Update is called once per frame
--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -5,18 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
+    [SerializeField] private Renderer _background;
+
     [SerializeField] private string _startGame;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        AudioManager.instance.PlayMusicClip(5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _background.material.mainTextureOffset = new Vector2(Time.time * 0.05f, 0f);
     }
 
     public void StartGame()
