0275aa070cc218007e3373d9a50a17f50797f5b3
har-y <jakub.harenda@gmail.com>
2021-02-14 19:40:23 +0100

    add level select unlocked levels

--- Assets/Scripts/Level Mechanics/MapPoint.cs
+++ Assets/Scripts/Level Mechanics/MapPoint.cs
@@ -13,11 +13,31 @@ public class MapPoint : MonoBehaviour
     public bool isLocked;
 
     public string levelToLoad;
+    public string levelToCheck;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isLevel && levelToLoad != null)
+        {
+            isLocked = true;
+
+            if (levelToCheck != null)
+            {
+                if (PlayerPrefs.HasKey(levelToCheck + "_unlocked"))
+                {
+                    if (PlayerPrefs.GetInt(levelToCheck + "_unlocked") == 1)
+                    {
+                        isLocked = false;
+                    }
+                }
+            }
+
+            if (levelToLoad == levelToCheck)
+            {
+                isLocked = false;
+            }
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -75,6 +75,8 @@ public class LevelManager : MonoBehaviour
 
         yield return new WaitForSeconds((1f / UIController.instance.fadeSpeed) + 0.2f);
 
+        PlayerPrefs.SetInt(SceneManager.GetActiveScene().name + "_unlocked", 1);
+
         SceneManager.LoadScene(_nextLevel);
     }
 }
