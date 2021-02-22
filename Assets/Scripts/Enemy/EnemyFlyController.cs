0fba489262012e919af8aa70dc4b58e57a4d7905
har-y <jakub.harenda@gmail.com>
2021-02-22 19:27:19 +0100

    add enemy eagle fly to player

--- Assets/Scripts/Enemy/EnemyFlyController.cs
+++ Assets/Scripts/Enemy/EnemyFlyController.cs
@@ -9,7 +9,15 @@ public class EnemyFlyController : MonoBehaviour
 
     [SerializeField] private float _enemySpeed;
 
+    [SerializeField] private float _attackDistance;
+    [SerializeField] private float _attackSpeed;
+    [SerializeField] private float _waitAfterAttack;
+
+    private Vector3 _attackTarget;
+
     private int _currentPoint;
+
+    private float _attackCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +28,42 @@ public class EnemyFlyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FlyingEnemy();
+        EnemyFlyAttack();
     }
 
+    private void EnemyFlyAttack()
+    {
+        if (_attackCounter > 0)
+        {
+            _attackCounter -= Time.deltaTime;
+        }
+        else
+        {
+            if (Vector3.Distance(_enemy.position, PlayerController.instance.transform.position) > _attackDistance)
+            {
+                _attackTarget = Vector3.zero;
 
-    private void FlyingEnemy()
+                EnemyFlyMove();
+            }
+            else
+            {
+                if (_attackTarget == Vector3.zero)
+                {
+                    _attackTarget = PlayerController.instance.transform.position;
+                }
+
+                _enemy.position = Vector3.MoveTowards(_enemy.position, _attackTarget, _attackSpeed * Time.deltaTime);
+
+                if (Vector3.Distance(_enemy.position, _attackTarget) <= 0.1f)
+                {
+                    _attackCounter = _waitAfterAttack;
+                    _attackTarget = Vector3.zero;
+                }
+            }
+        }
+    }
+
+    private void EnemyFlyMove()
     {
         _enemy.position = Vector3.MoveTowards(_enemy.position, _points[_currentPoint].position, _enemySpeed * Time.deltaTime);
 
