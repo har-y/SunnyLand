cae546327cf317c51b422b5ea4aac43505172f9d
har-y <jakub.harenda@gmail.com>
2021-02-23 23:17:43 +0100

    add: player death script

---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerHealthController.instance.currentHealth = 0;
+            UIController.instance.UpdateHealthDisplay();
+            LevelManager.instance.RespawnPlayer();
+        }
+    }
+}
