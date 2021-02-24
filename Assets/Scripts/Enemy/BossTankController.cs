962eea4be183244b82b1f1c8f0fca57eeecf7af2
har-y <jakub.harenda@gmail.com>
2021-02-24 23:52:56 +0100

    add big boss - tank hit, shoot, move state

--- Assets/Scripts/Enemy/BossTankController.cs
+++ Assets/Scripts/Enemy/BossTankController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class BossTankController : MonoBehaviour
 {
+    [Header("Boss Tank")]
+    private Animator _animator;
+    [SerializeField] private Transform _boss;
+    [SerializeField] private BossState _state;
+
+    [Header("Boss Tank - Movement")]
+    [SerializeField] private Transform _rightPoint;
+    [SerializeField] private Transform _leftPoint;
+    [SerializeField] private float _moveSpeed;
+    private bool _changeDirection;
+
+    [Header("Boss Tank - Bullet")]
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _bulletPoint;
+    [SerializeField] private float _bulletDelay;
+    private float _bulletCounter;
+
+    [Header("Boss Tank - Health")]
+    [SerializeField] private float _hurtTime;
+    private float _hurtCounter;
+
+    public enum BossState
+    {
+        move,
+        hit,
+        shoot
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+
+        _state = BossState.shoot;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (_state)
+        {
+            case BossState.move:
+                break;
+            case BossState.hit:
+                break;
+            case BossState.shoot:
+                break;
+            default:
+                break;
+        }
     }
 }
